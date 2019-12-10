47225491891c4981ffc79bb6c320f9686c15164e
sboronin <Sergey.Boronin@bia-tech.ru>
2019-12-10 19:08:03 +0300

    small fix

--- core-api/src/main/java/ru/boronin/core/api/schedulers/SchedulersProvider.kt
+++ core-api/src/main/java/ru/boronin/core/api/schedulers/SchedulersProvider.kt
@@ -2,9 +2,6 @@ package ru.boronin.core.api.schedulers
 
 import io.reactivex.Scheduler
 
-/**
- * Created by Igor Vasilev on 20.11.17.
- */
 interface SchedulersProvider {
   fun ui(): Scheduler
   fun computation(): Scheduler
