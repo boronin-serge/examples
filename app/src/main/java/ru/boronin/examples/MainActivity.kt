aec5cf7fdebd0871df352796a862a0ca2e92e5ce
sboronin <Sergey.Boronin@bia-tech.ru>
2019-12-17 19:00:52 +0300

    add splash-screen

--- app/src/main/java/ru/boronin/examples/MainActivity.kt
+++ app/src/main/java/ru/boronin/examples/MainActivity.kt
@@ -6,20 +6,34 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import com.boronin.coroutines.ExceptionsAndCancellation
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme) // Update splash theme
+        showAsFullscreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        button.setOnClickListener(this)
+        setContentView(R.layout.activity_main)
     }
 
-    override fun onClick(v: View?) {
-        when(v?.id) {
-            R.id.button -> {
-                ExceptionsAndCancellation().cancelWithTimeout()
-            }
-        }
+    /**
+     * Если нужно отобразить на сплэш-скрине анимацию, то нужно сделать следующее.
+     *  - В activity_main сделать такой же background как в теме сплеша.
+     *  - Добавить необходимую анимацию в разметку
+     *  - Сделать MainActivity полноэкранной перед super.onCreate(...) (чтобы совпасть со сплэшем):
+     *  [showAsFullscreen]
+     *  - По окончании анимации очистить бэкграунд, вернуть из полноэкранного режима:
+     *  [clearBackground]
+     */
+
+    private fun showAsFullscreen() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    }
+
+    private fun clearBackground() {
+        activityRootLayout.background = null
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_VISIBLE
     }
 }
