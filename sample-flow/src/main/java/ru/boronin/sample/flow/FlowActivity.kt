93dbd4d899fc90728c7181dcb8db8111244157e5
sboronin <Sergey.Boronin@bia-tech.ru>
2020-02-07 17:19:41 +0300

    add sample-flow module

---
+++ sample-flow/src/main/java/ru/boronin/sample/flow/FlowActivity.kt
@@ -0,0 +1,12 @@
+package ru.boronin.sample.flow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow)
+    }
+}
