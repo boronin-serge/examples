81f5a75e9d111741727dfb5a429978e0a88af141
sboronin <Sergey.Boronin@bia-tech.ru>
2020-02-21 12:57:35 +0300

    add make default folders script

---
+++ makefolders.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir common
+mkdir common/presentation
+mkdir common/presentation/mvp
+
+mkdir di
+
+mkdir model
+mkdir model/common
+
+mkdir features
+mkdir features/main  
+mkdir features/main/di
+mkdir features/main/navigator
+mkdir features/main/ui
+
+mv MainActivity.kt features/main/ui
+
+mkdir utils
+mkdir utils/helpers
+mkdir utils/managers
+mkdir utils/storage
+
+touch App.kt 
+
