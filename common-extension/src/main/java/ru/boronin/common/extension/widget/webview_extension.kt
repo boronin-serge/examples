57a52973cff0acdbb19224f0d0ccebae7f476ccc
Sergey <boronin-serge@ya.ru>
2020-04-13 12:59:43 +0300

    add webview extension

---
+++ common-extension/src/main/java/ru/boronin/common/extension/widget/webview_extension.kt
@@ -0,0 +1,20 @@
+package ru.boronin.common.extension.widget
+
+import android.annotation.SuppressLint
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+@SuppressLint("SetJavaScriptEnabled")
+fun WebView.getHtmlResponse(callback: (String) -> Unit) {
+  settings.javaScriptEnabled = true
+  webViewClient = object : WebViewClient() {
+    override fun onPageFinished(view: WebView?, url: String?) {
+      super.onPageFinished(view, url)
+      view?.evaluateJavascript(
+        "(function() { return ('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>'); })();"
+      ) { html ->
+        callback.invoke(html)
+      }
+    }
+  }
+}
